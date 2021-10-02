078135e0456d764d53b1afbf4d7186ccbf4bd6a4
Gabriele <gabrielepollice@gmail.com>
2021-10-02 10:55:16 +0200

    Added input check using regular expressions and silenced ffmpeg warnings

--- src/verthandi.py
+++ src/verthandi.py
@@ -3,11 +3,11 @@ from discord.ext import commands
 from discord import FFmpegPCMAudio
 from discord.ext.commands.errors import MissingRequiredArgument
 import streamlink
-
+import re
 bot = commands.Bot(command_prefix="$")
 
 def audioGenerator(url_stream):
-    ffmpeg_options = {'before_options':'-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5','options':'-vn'}
+    ffmpeg_options = {'before_options':'-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5','options':'-vn -hide_banner -loglevel error'}
     stream = streamlink.streams(url_stream)
     audio = stream["audio_only"]
     return FFmpegPCMAudio(audio.url,**ffmpeg_options)
@@ -16,6 +16,9 @@ def audioGenerator(url_stream):
 async def stream(ctx,msg):
     if ctx.message.author.voice==None:
         await ctx.send("You have to be in a voice channel in order to use this command")
+        return
+    elif re.fullmatch(r"https:\/\/www.twitch.tv\/.+",msg) == None:
+        await ctx.send("You have to use a twitch channel link")
         return
     voice_channel = ctx.message.author.voice.channel
     voice = discord.utils.get(ctx.guild.voice_channels,name=voice_channel.name)
